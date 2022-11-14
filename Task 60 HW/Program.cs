1c59794fbdc0d9faabb1437cdfac5d2103b5d11b
Daria <dasha_starikova@mail.ru>
2022-11-14 12:25:23 +0300

    Create HW Tasks S8

---
+++ Task 60 HW/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+/* Массив размером 2 x 2 x 2 */
+
+void InputMatrix(int[,,] MatrixOne)
+{
+    for (int i = 0; i < MatrixOne.GetLength(0); i++)
+    {
+        for (int j = 0; j < MatrixOne.GetLength(1); j++)
+        {
+            for (int k = 0; k < MatrixOne.GetLength(2); k++)
+
+            MatrixOne[i, j, k] = new Random().Next(1, 11);
+        }
+    }
+}
+void PrintMatrix(int[,,] MatrixOne)
+{
+    for (int i = 0; i < MatrixOne.GetLength(0); i++)
+    {
+        for (int j = 0; j < MatrixOne.GetLength(1); j++)
+        {
+            for (int k = 0; k < MatrixOne.GetLength(2); k++)
+            
+            Console.WriteLine(MatrixOne[i,j,k]+ "(" + i + ", " + j + ", " + k + ")");            
+            
+         }
+    }
+}
+
+
+int[,,] MatrixOne = new int[2,2,2];
+Console.WriteLine("Массив: ");
+InputMatrix(MatrixOne);
+PrintMatrix(MatrixOne);
---
+++ Task HW Razbor 58/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+void InputMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = new Random().Next(1, 11);
+Console.Write($"{matrix[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+void ProizvMetrixs (int[ , ] MatrixOne, int[ , ] MatrixTwo)
+{
+    Console.WriteLine();
+    int [,] ResultMatrix = new int [MatrixOne.GetLength(0), MatrixOne.GetLength(1)];
+    for (int i = 0; i < MatrixOne.GetLength(0); i++)
+   {
+        for (int j = 0; j < MatrixOne.GetLength(1); j++)
+        {
+            ResultMatrix[i,j] = MatrixOne[i,j] * MatrixTwo[i,j];
+            Console.Write($"{ResultMatrix[i,j]} \t");
+           
+        }
+        Console.WriteLine();
+   }
+}      
+
+Console.Clear();
+Console.Write("Введите кол-во строк и столбцов: ");
+string[] num = Console.ReadLine().Split(' ');
+int n = int.Parse(num[0]);
+int m = int.Parse(num[1]);
+int[,] MatrixOne = new int[n, m];
+int[,] MatrixTwo = new int[n, m];
+Console.WriteLine("Массив 1: ");
+InputMatrix(MatrixOne);
+Console.WriteLine("Массив 2: ");
+InputMatrix(MatrixTwo);
+Console.WriteLine("Массив 3 -Произведение массивов : ");
+ProizvMetrixs (MatrixOne, MatrixTwo);
